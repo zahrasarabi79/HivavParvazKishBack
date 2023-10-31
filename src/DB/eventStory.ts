b42666619a6ace099ef1499fe4322b994b972739
zahra sarabi <zahra.pouyagaran@gmail.com>
2023-10-31 16:02:14 +0330

    eventstory of reports

--- src/DB/eventStory.ts
+++ src/DB/eventStory.ts
@@ -19,15 +19,113 @@ export const updatedEventStory = (updatedContracts: IContractDto, existedContrac
       }
     }
   };
-  const compareDeleteObjects = (updatedObj: any, currentObj: any, reportIndex: any, reportPaymentIndex: any) => {
+  const compareReportsObjects = (updatedObj: any, currentObj: any, reportIndex: any) => {
+    console.log("1first", reportIndex);
+
     for (const key in updatedObj) {
       if (key !== "id" && key !== "contractId" && key !== "reportId") {
-        if (key === "datepayment") {
-          if (new Date(updatedObj[key]).getTime() !== new Date(currentObj?.[key] || 0).getTime()) {
-            events.push(Events.ReportPaymentUpdated(reportIndex, reportPaymentIndex, "datepayment"));
+        if (updatedObj[key] !== (currentObj?.[key] || 0)) {
+          switch (key) {
+            case "totalCost":
+              events.push(Events.ReportTotalCost(reportIndex));
+              break;
+
+            case "reportDescription":
+              events.push(Events.ReportReportDescription(reportIndex));
+              break;
+
+            case "presenter":
+              events.push(Events.ReportPresenter(reportIndex));
+              break;
+            case "reportsReturnPayment":
+              if (updatedContracts.reports[reportIndex].reportsReturnPayment.length > (existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.length || 0)) {
+                updatedContracts.reports[reportIndex].reportsReturnPayment.forEach((updatedObj, reportsReturnPaymentIndex) => {
+                  if (reportsReturnPaymentIndex < (existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.length || 0)) {
+                    const currentObj = existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.[reportsReturnPaymentIndex]?.dataValues;
+                    compareReturnObjects(updatedObj, currentObj, reportIndex, reportsReturnPaymentIndex);
+                  } else {
+                    events.push(Events.ReportReturnPaymentCreated(reportIndex, reportsReturnPaymentIndex));
+                  }
+                });
+              }
+              if (updatedContracts.reports[reportIndex].reportsReturnPayment.length < (existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.length || 0)) {
+                existedContract?.reports?.[reportIndex]?.reportsReturnPayment.forEach((currentObj, reportReturnPaymentIndex) => {
+                  if (reportReturnPaymentIndex < updatedContracts.reports[reportIndex].reportsReturnPayment.length) {
+                    const updatedObj = updatedContracts.reports[reportIndex].reportsReturnPayment[reportReturnPaymentIndex];
+                    compareReturnObjects(updatedObj, currentObj, reportIndex, reportReturnPaymentIndex);
+                  } else {
+                    events.push(Events.ReportReturnPaymentDeleted(reportIndex, reportReturnPaymentIndex));
+                  }
+                });
+              }
+              if (updatedContracts.reports[reportIndex].reportsReturnPayment.length === (existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.length || 0)) {
+                const CompareReturnPaymentsObjects = (currentArray: ReportsReturnPaymentModel[] | undefined, updatedArray: IReportsReturnPaymentDto[]) => {
+                  updatedArray.forEach((_, reportReturnPaymentIndex) => {
+                    const updatedObj: IReportsReturnPaymentDto = updatedArray?.[reportReturnPaymentIndex];
+                    const currentObj: IReportsReturnPayment | undefined = currentArray?.[reportReturnPaymentIndex]?.dataValues;
+                    for (const key in updatedObj) {
+                      if (key !== "id" && key !== "contractId" && key !== "reportId") {
+                        if (key === "dateReturnPayment") {
+                          if (new Date(updatedObj[key]).getTime() !== new Date(currentObj?.[key] || 0).getTime()) {
+                            events.push(Events.ReportReturnPaymentUpdated(reportIndex, reportReturnPaymentIndex, "dateReturnPayment"));
+                          }
+                        } else if (updatedObj[key] !== currentObj?.[key]) {
+                          events.push(Events.ReportReturnPaymentUpdated(reportIndex, reportReturnPaymentIndex, key));
+                          console.log(key, reportReturnPaymentIndex);
+                        }
+                      }
+                    }
+                  });
+                };
+                CompareReturnPaymentsObjects(existedContract?.reports?.[reportIndex]?.reportsReturnPayment, updatedContracts.reports[reportIndex]?.reportsReturnPayment);
+              }
+              break;
+            case "reportsPayment":
+              if (updatedContracts.reports[reportIndex].reportsPayment.length > (existedContract?.reports?.[reportIndex]?.reportsPayment?.length || 0)) {
+                updatedContracts.reports[reportIndex].reportsPayment.forEach((updatedObj, reportPaymentIndex) => {
+                  if (reportPaymentIndex < (existedContract?.reports?.[reportIndex]?.reportsPayment?.length || 0)) {
+                    const currentObj = existedContract?.reports?.[reportIndex]?.reportsPayment?.[reportPaymentIndex]?.dataValues;
+                    compareObjects(updatedObj, currentObj, reportIndex, reportPaymentIndex);
+                  } else {
+                    events.push(Events.ReportPaymentCreated(reportIndex, reportPaymentIndex));
+                  }
+                });
+              }
+              if (updatedContracts.reports[reportIndex].reportsPayment.length < (existedContract?.reports?.[reportIndex]?.reportsPayment?.length || 0)) {
+                existedContract?.reports?.[reportIndex]?.reportsPayment.forEach((currentObj, reportPaymentIndex) => {
+                  if (reportPaymentIndex < updatedContracts.reports[reportIndex].reportsPayment.length) {
+                    const updatedObj = updatedContracts.reports[reportIndex].reportsPayment[reportPaymentIndex];
+                    compareObjects(updatedObj, currentObj, reportIndex, reportPaymentIndex);
+                  } else {
+                    events.push(Events.ReportPaymentDeleted(reportIndex, reportPaymentIndex));
+                  }
+                });
+              }
+              if (updatedContracts.reports[reportIndex].reportsPayment.length === (existedContract?.reports?.[reportIndex]?.reportsPayment?.length || 0)) {
+                const ComparePaymentsObjects = (currentArray: ReportsPaymentModel[] | undefined, updatedArray: IReportPaymentDto[]) => {
+                  updatedArray.forEach((_, reportPaymentIndex) => {
+                    const updatedObj: IReportPaymentDto = updatedArray[reportPaymentIndex];
+                    const currentObj: IReportsPayment | undefined = currentArray?.[reportPaymentIndex]?.dataValues;
+                    for (const key in updatedObj) {
+                      if (key !== "id" && key !== "contractId" && key !== "reportId") {
+                        if (key === "datepayment") {
+                          if (new Date(updatedObj[key]).getTime() !== new Date(currentObj?.[key] || 0).getTime()) {
+                            events.push(Events.ReportPaymentUpdated(reportIndex, reportPaymentIndex, "datepayment"));
+                          }
+                        } else if (updatedObj[key] !== currentObj?.[key]) {
+                          events.push(Events.ReportPaymentUpdated(reportIndex, reportPaymentIndex, key));
+                        }
+                      }
+                    }
+                  });
+                };
+                ComparePaymentsObjects(existedContract?.reports?.[reportIndex].reportsPayment, updatedContracts.reports[reportIndex].reportsPayment);
+              }
+              break;
+            default:
+              "didnot set position ";
+              break;
           }
-        } else if (updatedObj[key] !== currentObj?.[key]) {
-          events.push(Events.ReportPaymentUpdated(reportIndex, reportPaymentIndex, key));
         }
       }
     }
@@ -59,11 +157,19 @@ export const updatedEventStory = (updatedContracts: IContractDto, existedContrac
     events.push(Events.ContractTypeContractUpdated);
   }
   if (updatedContracts.reports?.length > (existedContract?.reports?.length || 0)) {
-    events.push(Events.ReportCreated);
+    updatedContracts.reports.forEach((updatedReport, reportIndex) => {
+      const currentReport = existedContract?.reports?.[reportIndex]?.dataValues;
+      if (reportIndex < (existedContract?.reports?.length || 0)) {
+        compareReportsObjects(updatedReport, currentReport, reportIndex);
+      } else {
+        events.push(Events.ReportCreated);
+      }
+    });
   }
   if (updatedContracts.reports.length < (existedContract?.reports?.length || 0)) {
     events.push(Events.ReportDeleted);
-  } else {
+  }
+  if (updatedContracts.reports.length === (existedContract?.reports?.length || 0)) {
     updatedContracts.reports.forEach((_, reportIndex) => {
       if (updatedContracts.reports[reportIndex].totalCost !== (existedContract?.reports?.[reportIndex]?.totalCost || 0)) {
         events.push(Events.ReportTotalCost(reportIndex));
@@ -137,7 +243,7 @@ export const updatedEventStory = (updatedContracts: IContractDto, existedContrac
       if (updatedContracts.reports[reportIndex].reportsReturnPayment.length === (existedContract?.reports?.[reportIndex]?.reportsReturnPayment?.length || 0)) {
         const CompareReturnPaymentsObjects = (currentArray: ReportsReturnPaymentModel[] | undefined, updatedArray: IReportsReturnPaymentDto[]) => {
           updatedArray.forEach((_, reportReturnPaymentIndex) => {
-            const updatedObj: IReportsReturnPaymentDto = updatedArray[reportReturnPaymentIndex];
+            const updatedObj: IReportsReturnPaymentDto = updatedArray?.[reportReturnPaymentIndex];
             const currentObj: IReportsReturnPayment | undefined = currentArray?.[reportReturnPaymentIndex]?.dataValues;
             for (const key in updatedObj) {
               if (key !== "id" && key !== "contractId" && key !== "reportId") {
@@ -153,7 +259,7 @@ export const updatedEventStory = (updatedContracts: IContractDto, existedContrac
             }
           });
         };
-        CompareReturnPaymentsObjects(existedContract?.reports?.[reportIndex].reportsReturnPayment, updatedContracts.reports[reportIndex].reportsReturnPayment);
+        CompareReturnPaymentsObjects(existedContract?.reports?.[reportIndex]?.reportsReturnPayment, updatedContracts.reports[reportIndex]?.reportsReturnPayment);
       }
     });
   }
